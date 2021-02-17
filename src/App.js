7183a7fd33c6acc328bbf9f51fd3111065f5fa44
sudhakarsn15 <35915646+sudhakarsn15@users.noreply.github.com>
2021-02-17 17:08:12 +0530

    Completed code Finalized code and its working fine.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,27 @@ export default class AppDragDropDemo extends Component {
       { name: "Get breakfast", category: "todo", bgcolor: "#96D1CD" }
     ]
   };
+  onDragStart = (ev, id) => {
+    console.log("dragstart:", id);
+    ev.dataTransfer.setData("id", id);
+  };
+  onDragOver = (ev) => {
+    ev.preventDefault();
+  };
+  onDrop = (ev, cat) => {
+    let id = ev.dataTransfer.getData("id");
+    let tasks = this.state.tasks.filter((task) => {
+      if (task.name === id) {
+        task.category = cat;
+      }
+      return task;
+    });
 
+    this.setState({
+      ...this.state,
+      tasks
+    });
+  };
   render() {
     var tasks = {
       todo: [],
@@ -21,6 +41,7 @@ export default class AppDragDropDemo extends Component {
           key={t.name}
           className="draggable"
           draggable
+          onDragStart={(e) => this.onDragStart(e, t.name)}
           style={{ backgroundColor: t.bgcolor }}
         >
           {t.name}
@@ -31,12 +52,20 @@ export default class AppDragDropDemo extends Component {
     return (
       <div className="container-drag">
         <h2 className="header">DRAG & DROP DEMO</h2>
-        <div className="todo">
+        <div
+          className="todo"
+          onDragOver={(ev) => this.onDragOver(ev)}
+          onDrop={(e) => this.onDrop(e, "todo")}
+        >
           <span className="task-header">To-Do List</span>
           {tasks.todo}
         </div>
-        <div className="droppable" onDragOver={(ev) => this.onDragOver(ev)}>
-          ><span className="task-header">Tasks Done</span>
+        <div
+          className="droppable"
+          onDragOver={(ev) => this.onDragOver(ev)}
+          onDrop={(e) => this.onDrop(e, "done")}
+        >
+          <span className="task-header">Tasks Done</span>
           {tasks.done}
         </div>
       </div>
